e985836dc2419e5492f3c0e84db70b176e6889e8
nyanishinde <nyanishinde10@gmail.com>
2024-12-10 19:40:17 +0530

    Completed the adapter and just modifing button is left

--- app/src/main/java/com/example/radiate/OnboardingAdapter.kt
+++ app/src/main/java/com/example/radiate/OnboardingAdapter.kt
@@ -1,29 +1,29 @@
 package com.example.radiate
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.radiate.databinding.OnboardingPagesBinding
 
-class OnboardingAdapter(private val items:List<OnboardingItemsDC>)
-    : RecyclerView.Adapter<OnboardingAdapter.OnboardingViewHolder>(){
+class OnboardingAdapter(private val items:List<OnboardingItemsDC>):RecyclerView.Adapter<OnboardingAdapter.OnboardingViewHolder>() {
+
+    inner class OnboardingViewHolder(private val binding: OnboardingPagesBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(itemsDC: OnboardingItemsDC){
+            binding.imageViewOnboardings.setImageResource(itemsDC.imageResId)
+            binding.textViewTitle.text = itemsDC.title
+            binding.textViewDescription.text = itemsDC.description
+        }
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnboardingAdapter.OnboardingViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.onboarding_pages,parent,false)
-        return OnboardingViewHolder(view)
+        val binding = OnboardingPagesBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return OnboardingViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: OnboardingAdapter.OnboardingViewHolder, position: Int) {
         holder.bind(items[position])
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
-
-    inner class OnboardingViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView) {
-        fun bind(item: OnboardingItemsDC){
-            itemView.
-        }
-    }
+    override fun getItemCount(): Int = items.size
 }
--- app/src/main/java/com/example/radiate/OnboardingPages.kt
+++ app/src/main/java/com/example/radiate/OnboardingPages.kt
@@ -1,20 +1,63 @@
 package com.example.radiate
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 
 class OnboardingPages : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding_pages)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val items = listOf(
+            OnboardingItemsDC(R.drawable.onboardingjournal,"Journal Your Emotions","Effortlessly track your moods and moments. Discover patterns and nurture self-awareness with our journal feature."),
+            OnboardingItemsDC(R.drawable.onboardingmeditation,"Guided Meditation for Calm","Find peace with guided meditation and breathing exercises to relieve stress and enhance mindfulness."),
+            OnboardingItemsDC(R.drawable.onboardingblogs,"Discover Mental Wellness Blogs","Explore curated blogs on mental health, packed with insights and tips to boost resilience and well-being."),
+            OnboardingItemsDC(R.drawable.onboardinggroup,"Connect with Care","Join a supportive community to share and grow anonymously. Build meaningful connections without fear of judgment.")
+        )
+
+        val adapter = OnboardingAdapter(items)
+        val viewpager = findViewById<ViewPager2>(R.id.viewPager)
+        viewpager.adapter=adapter
+
+        setUpIndicators(items.size)
+        setCurrentIndicator(0)
+
+        viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                setCurrentIndicator(position)
+            }
+        })
+
+    }
+
+    private fun setCurrentIndicator(index: Int) {
+        val childCount = findViewById<LinearLayout>(R.id.indicatorLayout).childCount
+        for (i in 0 until childCount){
+            val imageView = findViewById<LinearLayout>(R.id.indicatorLayout).getChildAt(i) as ImageView
+            if(i == index){
+                imageView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.indicator_active))
+            }else{
+                imageView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.indicator_inactive))
+            }
+        }
+    }
+
+    private fun setUpIndicators(count: Int) {
+        val indicators = Array(count){ImageView(this)}
+        val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        layoutParams.setMargins(8,0,8,0)
+        indicators.forEach { indicator->
+            indicator.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.indicator_inactive))
+            indicator.layoutParams = layoutParams
+            findViewById<LinearLayout>(R.id.indicatorLayout).addView(indicator)
         }
     }
 }
