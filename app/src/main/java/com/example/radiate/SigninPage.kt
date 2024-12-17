85c3de11b52e84c81e8ed15077d7c927a26b43b3
nyanishinde <nyanishinde10@gmail.com>
2024-12-17 20:23:26 +0530

    completed activity_forget_password.xml

--- app/src/main/java/com/example/radiate/SigninPage.kt
+++ app/src/main/java/com/example/radiate/SigninPage.kt
@@ -1,6 +1,8 @@
 package com.example.radiate
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,13 @@ class SigninPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin_page)
+
+        val btnforgetPassword = findViewById<TextView>(R.id.txtForgetPassword)
+
+
+        btnforgetPassword.setOnClickListener {
+            startActivity(Intent(this,ForgetPassword::class.java))
+        }
 
     }
 }
