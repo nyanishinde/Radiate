2ca9e63be7ba8144ca77dac1f6c0006a3ecad869
nyanishinde <nyanishinde10@gmail.com>
2024-12-27 11:34:17 +0530

    added menu fragments

--- app/src/main/java/com/example/radiate/SigninPage.kt
+++ app/src/main/java/com/example/radiate/SigninPage.kt
@@ -2,22 +2,23 @@ package com.example.radiate
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SigninPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin_page)
 
-        val btnforgetPassword = findViewById<TextView>(R.id.txtForgetPassword)
+        val btnForgetPassword = findViewById<TextView>(R.id.txtForgetPassword)
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
 
-
-        btnforgetPassword.setOnClickListener {
+        btnForgetPassword.setOnClickListener {
             startActivity(Intent(this,ForgetPassword::class.java))
+        }
+        btnSignIn.setOnClickListener {
+            startActivity(Intent(this,HomePage::class.java))
         }
 
     }
