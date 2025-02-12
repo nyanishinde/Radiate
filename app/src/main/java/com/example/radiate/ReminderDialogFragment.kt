ce2cb72d49e89df2cd56a20dd7ee17a67392a0e2
nyanishinde <nyanishinde10@gmail.com>
2025-02-12 09:30:41 +0530

    completed the backend of ReminderDialogFragment.kt

--- app/src/main/java/com/example/radiate/ReminderDialogFragment.kt
+++ app/src/main/java/com/example/radiate/ReminderDialogFragment.kt
@@ -1,17 +1,17 @@
 package com.example.radiate
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.EditText
-import android.widget.Spinner
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import java.util.*
 import androidx.fragment.app.DialogFragment
-import org.w3c.dom.Text
-
 class ReminderDialogFragment:DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -24,6 +24,44 @@ class ReminderDialogFragment:DialogFragment() {
         val reminderTime = view.findViewById<EditText>(R.id.edtReminderTime)
         val btnCancel = view.findViewById<TextView>(R.id.btnCancel)
         val btnDone = view.findViewById<TextView>(R.id.btnDone)
+        val imgBtnCalender = view.findViewById<ImageButton>(R.id.imgBtnCalender)
+        val imgBtnTime = view.findViewById<ImageButton>(R.id.imgBtnTime)
+
+        // creating instance of calender to get current date and time
+        val calender = Calendar.getInstance()
+
+        val defaultDate = String.format("%d/%d/%d",calender.get(Calendar.DAY_OF_MONTH),calender.get(Calendar.MONTH)+1,calender.get(Calendar.YEAR))
+        reminderDate.setHint(defaultDate)
+        // Date picker dialog
+        imgBtnCalender.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(
+                requireContext(),
+                {_,year,month,dayOfMonth ->
+                    val selectedDate = "$dayOfMonth/${month + 1}/$year"
+                    reminderDate.setText(selectedDate)
+                },
+                calender.get(Calendar.YEAR),
+                calender.get(Calendar.MONTH),
+                calender.get(Calendar.DAY_OF_MONTH)
+            )
+            datePickerDialog.show()
+        }
+
+        val defaultTime = String.format("%02d:%02d",calender.get(Calendar.HOUR_OF_DAY),calender.get(Calendar.MINUTE))
+        reminderTime.setHint(defaultTime)
+        imgBtnTime.setOnClickListener {
+            val timePickerDialog = TimePickerDialog(
+                requireContext(),
+                {_,hourOfDay, minute ->
+                    val selectedTime = String.format("%02d:%02d", hourOfDay, minute)
+                    reminderTime.setText(selectedTime)
+                },
+                calender.get(Calendar.HOUR_OF_DAY),
+                calender.get(Calendar.MINUTE),
+                true
+            )
+            timePickerDialog.show()
+        }
 
         btnCancel.setOnClickListener {
             dismiss()
